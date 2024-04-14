ff3f34891a89f768d7d554d1c92e0c4d60b25792
srimantasahu <srimant.sahu@gmail.com>
2024-04-14 22:31:02 +0530

    updated query for ltp

--- src/main/resources/scripts/query.sql
+++ src/main/resources/scripts/query.sql
@@ -53,51 +53,55 @@
 
 -- Query: stocks near to low_52w with low pe value and high earnings per share
 
--- microcap: pe<5 and sect_index is null or NA, order by ltp
+-- microcap: pe<5, ltp<50, order: ltp
 
 select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, tot_mar_cap_cr, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
 where 1=1
-and (symbol_pe is null or symbol_pe < 5) 
-and (sect_index is null or sect_index = 'NA') 
-and ltp < (low_52w + high_52w)/2 
+and ltp < 50
+and (symbol_pe is null or symbol_pe < 5)
+and (adjusted_pe is null or adjusted_pe < 5)
 and tot_mar_cap_cr < (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 500)
 order by 
 ltp asc;
 
 
--- smallcap: pe<10 and +eps, order by ltp - low_52w
+-- smallcap: pe<15, ltp<150, order: ltp-low_52w
 
 select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, tot_mar_cap_cr, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
 where 1=1
-and (symbol_pe is null or symbol_pe < 10) 
-and (earnings_share is null or earnings_share > 0) 
+and ltp < 150
 and ltp < (low_52w + high_52w)/2 
+and (symbol_pe is null or symbol_pe < 15)
+and (adjusted_pe is null or adjusted_pe < 15)
 and tot_mar_cap_cr < (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 250)
 and tot_mar_cap_cr >= (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 500)
 order by  
 ltp - low_52w asc;
 
 
--- midcap: pe<15 and +eps, order by eps
+-- midcap: pe<25, ltp<250 and +eps, order: eps
 
 select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, tot_mar_cap_cr, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
 where 1=1
-and (symbol_pe is null or symbol_pe < 15) 
-and (earnings_share is null or earnings_share > 0) 
+and ltp < 250
 and ltp < (low_52w + high_52w)/2 
+and (symbol_pe is null or symbol_pe < 25) 
+and (adjusted_pe is null or adjusted_pe < 25)
+and (earnings_share is null or earnings_share > 0) 
 and tot_mar_cap_cr < (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 100)
 and tot_mar_cap_cr >= (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 250)
 order by 
 earnings_share desc;
 
-
--- largecap: pe<20 and +eps, order by 365d change
+-- largecap: pe<30, ltp<500 and +eps, order: 365d change
 
 select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, tot_mar_cap_cr, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
 where 1=1
-and (symbol_pe is null or symbol_pe < 20) 
-and (earnings_share is null or earnings_share > 0) 
+and ltp < 500
 and ltp < (low_52w + high_52w)/2 
+and (symbol_pe is null or symbol_pe < 30) 
+and (adjusted_pe is null or adjusted_pe < 30)
+and (earnings_share is null or earnings_share > 0) 
 and tot_mar_cap_cr >= (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 100)
 order by 
 per_chng_365d asc;
