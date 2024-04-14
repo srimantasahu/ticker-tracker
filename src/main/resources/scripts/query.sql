52b40117d676c26480590f6df2f9e7676c35786c
srimantasahu <srimant.sahu@gmail.com>
2024-04-14 14:32:54 +0530

    updated query for market caps

--- src/main/resources/scripts/query.sql
+++ src/main/resources/scripts/query.sql
@@ -11,6 +11,8 @@
 
 -- select status, count(*) from stocks.refdata group by status order by count desc
 
+-- select rank_number, symbol, tot_mar_cap_cr from (select *, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number <= 10
+
 
 ------------------------------------------------------------------------------------------------------------------------
 ------------------------------------------------------------------------------------------------------------------------
@@ -51,15 +53,54 @@
 
 -- Query: stocks near to low_52w with low pe value and high earnings per share
 
--- pe<20 and +eps, order by ltp
+-- microcap: pe<5 and sect_index is null or NA, order by ltp
 
--- select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
--- where symbol_pe < 20 and earnings_share > 0 and ltp < (low_52w + high_52w)/2 order by ltp asc, earnings_share desc, ltp asc, sect_index asc
+select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, tot_mar_cap_cr, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
+where 1=1
+and (symbol_pe is null or symbol_pe < 5) 
+and (sect_index is null or sect_index = 'NA') 
+and ltp < (low_52w + high_52w)/2 
+and tot_mar_cap_cr < (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 500)
+order by 
+ltp asc;
 
--- pe<10 and +eps, order by eps
 
--- select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
--- where symbol_pe < 10 and earnings_share > 0 and ltp < (low_52w + high_52w)/2 order by earnings_share desc, ltp asc, sect_index asc
+-- smallcap: pe<10 and +eps, order by ltp - low_52w
+
+select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, tot_mar_cap_cr, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
+where 1=1
+and (symbol_pe is null or symbol_pe < 10) 
+and (earnings_share is null or earnings_share > 0) 
+and ltp < (low_52w + high_52w)/2 
+and tot_mar_cap_cr < (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 250)
+and tot_mar_cap_cr >= (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 500)
+order by  
+ltp - low_52w asc;
+
+
+-- midcap: pe<15 and +eps, order by eps
+
+select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, tot_mar_cap_cr, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
+where 1=1
+and (symbol_pe is null or symbol_pe < 15) 
+and (earnings_share is null or earnings_share > 0) 
+and ltp < (low_52w + high_52w)/2 
+and tot_mar_cap_cr < (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 100)
+and tot_mar_cap_cr >= (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 250)
+order by 
+earnings_share desc;
+
+
+-- largecap: pe<20 and +eps, order by 365d change
+
+select sect_index, symbol, ltp, high_52w, low_52w, adjusted_pe, symbol_pe, earnings_share, promoter_holding, public_holding, per_chng_365d, per_chng_30d, tot_mar_cap_cr, face_val, price_band, buy_qty, sell_qty from stocks.refdata 
+where 1=1
+and (symbol_pe is null or symbol_pe < 20) 
+and (earnings_share is null or earnings_share > 0) 
+and ltp < (low_52w + high_52w)/2 
+and tot_mar_cap_cr >= (select tot_mar_cap_cr from (select tot_mar_cap_cr, rank() over (order by tot_mar_cap_cr desc) rank_number from stocks.refdata where tot_mar_cap_cr is not null) rt where rank_number = 100)
+order by 
+per_chng_365d asc;
 
 
 ------------------------------------------------------------------------------------------------------------------------
